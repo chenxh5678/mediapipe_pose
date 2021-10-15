94335ab6f14e9cc8d3ecf8a2b9f4257df7adc435
chenxh5678 <786028450@qq.com>
2021-10-15 17:59:04 +0800

    none
    none

--- pose.py
+++ pose.py
@@ -1,23 +1,20 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
 mp_drawing = mp.solutions.drawing_utils
 myPose = mp.solutions.pose
 poses = myPose.Pose()
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('demo.mp4')
 while True:
     ret,frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = poses.process(frame)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     if results.pose_landmarks:  # 包含姿势地标的“姿势地标”字段。
-        # get landmarks as numpy
+        # 获取坐标
         # landmarks = results.pose_landmarks.landmark
         # np_landmarks = np.array([(lm.x, lm.y, lm.z, lm.visibility) for lm in landmarks])
-        # print(np_landmarks.shape)
-        # print(np_landmarks)  # 获取坐标
+        # print(np_landmarks)
         # 画图
         mp_drawing.draw_landmarks(frame,results.pose_landmarks,myPose.POSE_CONNECTIONS)
     # if results.pose_world_landmarks:  # “pose_world_landmarks”（姿势世界地标）字段，包含真实世界三维坐标中的姿势地标，以米为单位，原点位于臀部之间的中心。
